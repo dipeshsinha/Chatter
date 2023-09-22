fdc6e59d777ce008377b98ac497332ba226d1c45
Dipesh Sinha <1606270@kiit.ac.in>
2023-09-22 17:36:07 +0530

    seconf commit

--- src/main/java/com/springproj/chatter/chat/ChatController.java
+++ src/main/java/com/springproj/chatter/chat/ChatController.java
@@ -5,9 +5,16 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ChatController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+    return new ModelAndView("index.html");
+    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
